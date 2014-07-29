3b64ec79bfd971203e273e7b30fe4471d043bfb9
sanj silva <sanj.silva2@sensis.com.au>
2014-07-29 15:39:08 +1000

    inital spring boots setup with required dependency

---
+++ src/main/java/au/com/vastech/photolab/ServletInitializer.java
@@ -0,0 +1,13 @@
+package au.com.vastech.photolab;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
